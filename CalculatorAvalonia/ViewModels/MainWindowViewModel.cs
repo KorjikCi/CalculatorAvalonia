79ad21a230f92b070fc70dd6cb813004465fb6db
KorjikCi <koorjik94@gmail.com>
2022-10-21 02:01:48 +0300

    Added buttons clear, all clear and result

--- CalculatorAvalonia/ViewModels/MainWindowViewModel.cs
+++ CalculatorAvalonia/ViewModels/MainWindowViewModel.cs
@@ -11,7 +11,34 @@ namespace CalculatorAvalonia.ViewModels
         public MainWindowViewModel()
         {
             SetInputSymbolCommand = new DelegateCommand<string?>(SetSymbol);
+            CalcCommand = new DelegateCommand(OnCalc);
+            ClearLastCommand = new DelegateCommand(OnClearLastSymbol);
+            AllClearCommand = new DelegateCommand(AllClear);
         }
+
+        private void OnCalc()
+        {
+            DisplayText = "Result";
+        }
+
+        private void OnClearLastSymbol()
+        {
+            if (!string.IsNullOrWhiteSpace(DisplayText))
+            {
+                DisplayText = DisplayText[..^1];
+                //DisplayText = DisplayText.Substring(0, DisplayText.Length - 1);
+                if (string.IsNullOrWhiteSpace(DisplayText))
+                {
+                    AllClear();
+                }
+            }
+        }
+
+        private void AllClear()
+        {
+            DisplayText = "0";
+        }
+
         public string DisplayText
         {
             get => _displayText;
@@ -23,10 +50,23 @@ namespace CalculatorAvalonia.ViewModels
         }
 
         public ICommand SetInputSymbolCommand { get; }
+        public ICommand CalcCommand { get; }
+        public ICommand ClearLastCommand { get; }
+        public ICommand AllClearCommand { get; }
         
         private void SetSymbol(string? parameter)
         {
+            if (DisplayText == "0" && IsDigit(parameter)) //Убираем дефолтный ноль с дисплея, когда начинаем вводить символы
+            {
+                DisplayText = string.Empty;
+            }
+            
             DisplayText += parameter;
+        }
+
+        private static bool IsDigit(string? parameter)
+        {
+            return int.TryParse(parameter, out var d);
         }
     }
 }
